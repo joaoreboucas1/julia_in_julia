fe6e102d5db7fe471d5bb88cceb6748514f78838
João Victor Rebouças <65295550+joaoreboucas1@users.noreply.github.com>
2023-04-23 17:09:58 -0300

    Adding two example visualizations of Mandelbrot set

--- julia.jl
+++ julia.jl
@@ -1,4 +1,16 @@
+using Plots
+using Colors
+
+# Plot options
 const global MAX_ITERATIONS = 20
+const global NX = 1000
+const global NY = 1000
+const global XMIN = -2
+const global XMAX = 1
+const global YMIN = -1.2
+const global YMAX = 1.2
+delta_x = (xmax - xmin)/Nx
+delta_y = (ymax - ymin)/Ny
 
 function f(z, c)
     z^2 + c
@@ -15,16 +27,18 @@ function is_in_mandelbrot(z)
     return true
 end
 
-z_values = [
-    1,
-    im,
-    -1
-]
-
-for z in z_values
+color = zeros((Ny,Nx))
+for i = 1:Nx, j = 1:Ny
+    x = xmin + delta_x*(i-1)
+    y = ymin + delta_y*(j-1)
+    z = x + y*1im
     if is_in_mandelbrot(z)
-        println("The number $z is in the Mandelbrot set")
+        print("The number $z is in the Mandelbrot set\n")
+        color[j,i] = 1
     else
-        println("The number $z is not in the Mandelbrot set")
+        print("The number $z is not in the Mandelbrot set\n")
+        color[j,i] = 0
     end
 end
+heatmap(color, color=:greys)
+savefig("test.pdf")
