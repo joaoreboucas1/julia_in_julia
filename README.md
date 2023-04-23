e47ac4cdb6e419f09ce97a5ce0a5102ec38be040
João Victor Rebouças <65295550+joaoreboucas1@users.noreply.github.com>
2023-04-23 19:38:41 -0300

    Implemented Julia sets, examples in Figures/, command-line usage

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Julia in Julia
-(WIP) A simple visual representation of the Mandelbrot set using `Julia`. The Mandelbrot set is related to the Julia set, so...
+(WIP) A simple visual representation of the Mandelbrot and Julia sets.
 
-Next step: actually generate Julia set visualizations.
+## Usage
+`$ julia julia.jl set n iter xmin xmax ymin ymax out_file`
+ - `set`: either `mandelbrot` or `julia`
+ - `n`: controls the number of points in each axis
+ - `iter`: controls the number of function iterations to check if `abs(z) < 2`
+ - `xmin`, `xmax`, `ymin`, `ymax`: the axis limits
+ - `out_file`: the name of the output figure
--- julia.jl
+++ julia.jl
@@ -1,44 +1,60 @@
 using Plots
 using Colors
 
-# Plot options
-const global MAX_ITERATIONS = 20
-const global NX = 1000
-const global NY = 1000
-const global XMIN = -2
-const global XMAX = 1
-const global YMIN = -1.2
-const global YMAX = 1.2
-delta_x = (xmax - xmin)/Nx
-delta_y = (ymax - ymin)/Ny
+# Parse command-line options
+try
+    const global which_set = ARGS[1]
+    const global N = parse(Int64, ARGS[2])
+    const global MAX_ITERATIONS = parse(Int64, ARGS[3])
+    const global XMIN = parse(Float64, ARGS[4])
+    const global XMAX = parse(Float64, ARGS[5])
+    const global YMIN = parse(Float64, ARGS[6])
+    const global YMAX = parse(Float64, ARGS[7])
+    const global OUT_NAME = ARGS[8]
+catch Exception
+    println("Usage: julia $PROGRAM_NAME set n iter xmin xmax ymin ymax out_name")
+    println("    set: either mandelbrot or julia")
+    println("    n: the number of points in each axis")
+    println("    iter: the number of iterations to check if abs(z)<2")
+    println("    xmin, xmax, ymin, ymax: axes limits")
+    println("    out_name: name of output file")
+    exit(1)
+end
+
+println("Making visualization of $which_set")
+
+
+delta_x = (XMAX - XMIN)/N
+delta_y = (YMAX - YMIN)/N
 
 function f(z, c)
     z^2 + c
 end
 
-function is_in_mandelbrot(z)
+function is_in_julia(z, c)
     cur = z
     for i = 1:MAX_ITERATIONS
-        cur = f(cur, z)
+        cur = f(cur, c)
         if abs(cur) > 2
-            return false
+            return i
         end
     end
-    return true
+    return 0
 end
 
-color = zeros((Ny,Nx))
-for i = 1:Nx, j = 1:Ny
-    x = xmin + delta_x*(i-1)
-    y = ymin + delta_y*(j-1)
-    z = x + y*1im
-    if is_in_mandelbrot(z)
-        print("The number $z is in the Mandelbrot set\n")
-        color[j,i] = 1
-    else
-        print("The number $z is not in the Mandelbrot set\n")
-        color[j,i] = 0
-    end
+function is_in_mandelbrot(z)
+    is_in_julia(z, z)
 end
+
+c = -0.4-0.6*1im
+color = zeros(UInt8, (N,N))
+for i = 1:N, j = 1:N
+    x = XMIN + delta_x*(i-1)
+    y = YMIN + delta_y*(j-1)
+    z = x + y*1im
+    color[j,i] = (which_set=="mandelbrot") ? is_in_mandelbrot(z) : is_in_julia(z,c)
+end
+println("Checked complex numbers. Saving plot...")
 heatmap(color, color=:greys)
-savefig("test.pdf")
+savefig(OUT_NAME)
+println("Saved visualization in $OUT_NAME")
